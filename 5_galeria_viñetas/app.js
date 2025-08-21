430b9a0070a844acf319a9443d2e219f873a46f2
Anlly <anllyotalvarosoto@gmail.com>
2025-08-21 15:41:28 -0500

    Galería viñetas segunda parte

--- 5_galeria_viñetas/app.js
+++ 5_galeria_viñetas/app.js
@@ -2,9 +2,14 @@ const grillos = document.querySelectorAll (".grin")
 const manzanas = document.querySelectorAll (".apple")
 const contadorgrillos = document.querySelector("#contador-grillos")
 const contadormanzanas = document.querySelector("#contador-manzanas")
+
 const playAudio = document.querySelector("#play-audio")
 const stopAudio = document.querySelector("#stop-audio")
 const audioFondo = document.querySelector("#audio-fondo")
+
+const playStopG1 = document.querySelector("#play-stop-g1")
+const audioG1 = document.querySelector("audio-g1")
+
 let totalgrillos = 0
 let totalmanzanas = 0
 
@@ -54,3 +59,33 @@ function pausarAudio () {
    audioFondo.pause()
 }
 stopAudio.addEventListener("click", pausarAudio)
+
+
+
+
+
+/* funcion botón siguiente */
+const prev = document.querySelector(".prev")
+const next = document.querySelector(".next")
+const escena1 = document.querySelector(".escena-1")
+const escena2 = document.querySelector(".escena-2")
+const escena3 = document.querySelector(".escena-3")
+
+let i = 1
+
+next.addEventListener("click", function () {
+  if(i === 1){
+    escena1.classList.add ("d-none")
+    escena2.classList.remove ("d-none")
+    i++ 
+  }else if (i == 2){
+    escena2.classList.add ("d-none")
+    escena3.classList.remove ("d-none")
+    i++
+  }
+})
+
+
+
+
+
